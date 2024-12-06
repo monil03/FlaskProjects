82397ecffb7aa8e11cad0ec0aa26100f9af20afc
monil03 <60208695+monil03@users.noreply.github.com>
2024-12-06 23:11:25 +0530

    Update main.py

--- Expense_Manager/main.py
+++ Expense_Manager/main.py
@@ -1,11 +1,12 @@
-from flask import Flask,request,render_template
+from flask import Flask,request,render_template,url_for,redirect
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager,login_user,logout_user,UserMixin
+from flask_login import LoginManager,login_user,logout_user,UserMixin,current_user
+from forms import expenseForm
 
 app=Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///databasename.db'
-app.config["SECRET_KEY"] = "secret_key"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config["SECRET_KEY"] = "abc"
 
 
 db=SQLAlchemy(app)
@@ -17,6 +18,17 @@ class user(db.Model,UserMixin):
     username = db.Column(db.String(100),nullable=False)
     emailid=db.Column(db.String(100),nullable=False)
     password=db.Column(db.String(100),nullable=False)
+    expenses = db.relationship('expenses', backref='user', lazy=True)
+
+
+class expenses(db.Model,UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    expense=db.Column(db.String(100),nullable=False)
+    date=db.Column(db.Date,nullable=False)
+    description=db.Column(db.String(500),nullable=False)
+    category=db.Column(db.String(100),nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
 
 @app.route('/signup',methods=['GET','POST'])
 def signup():
@@ -26,8 +38,7 @@ def signup():
         emailid=request.form.get('emailid')
         new_user=user(username=username,emailid=emailid,password=password)
         db.session.add(new_user)
-        db.session.commit()
-        
+        db.session.commit()  
         abc = user.query.all()
         for i in abc:
 
@@ -49,18 +60,37 @@ def login():
         if abc.password==password:
             login_user(abc)
 
-            return render_template('home.html',username=user_name)
+            return redirect(url_for('add'))
     return render_template('login.html')
 
 @login_manager.user_loader
 def loader_user(user_id):
     return user.query.get(user_id)
+
+@app.route('/add',methods=['GET','POST'])
+def add():
+    form=expenseForm()
+    if request.method=='POST':
+        ex= form.expense.data
+        d = form.date.data
+        c = form.category.data
+        desc = form.description.data  
+        print(ex)
+        new_expense=expenses(expense=ex,date=d,category=c,description=desc,user_id=current_user.id)  
+        db.session.add(new_expense)
+        db.session.commit()
+        abc = expenses.query.all()
+        for i in abc:
+            print(i.expense)
+        return "doneeeeeeeeeeeeeeeeeeee"
+    return render_template('add.html',form=form)
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
+ 
 with app.app_context():
     db.create_all()
 app.run(debug=True)
-
-
